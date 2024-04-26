bdcfbab9a3109dd2e72651a12987aa74290f6b56
Babenko Liudmyla <savelieva0509@gmail.com>
2024-04-26 15:02:03 +0300

    add  TaskList

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,10 +1,30 @@
+import { MdClose } from 'react-icons/md';
+import css from './Task.module.scss';
 
+type Task = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
 
-const Task = () => {
-  
+type TaskProps = {
+  task: Task;
+};
+
+const Task = ({ task }: TaskProps) => {
   return (
-   <>Task</>
-  )
-}
+    <div className={css.wrapper}>
+      <input
+        type="checkbox"
+        className={css.checkbox}
+        checked={task.completed}
+      />
+      <p className={css.text}>{task.text}</p>
+      <button className={css.btn}>
+        <MdClose size={24} />
+      </button>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,5 +1,26 @@
+import Task from '../Task/Task';
+import css from './TaskList.module.scss';
+
+const tasksInitialState = [
+  { id: 0, text: 'Do the dishes', completed: true },
+  { id: 1, text: 'Take out the trash', completed: true },
+  { id: 2, text: 'Clean the house', completed: false },
+  { id: 3, text: 'Organize the closet', completed: false },
+  { id: 4, text: 'Cook a delicious meal', completed: false },
+];
+
+
+
 const TaskList = () => {
-  return <>TaskList</>;
+  return (
+    <ul className={css.list}>
+      {tasksInitialState.map(task => (
+        <li className={css.listItem} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default TaskList;
