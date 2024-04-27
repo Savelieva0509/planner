4f24c4d4ebfba870a9b3d6694f79db13d0a9b62a
Savelieva0509 <104641072+Savelieva0509@users.noreply.github.com>
2024-04-27 18:40:42 +0000

    Deploying to gh-pages from @ Savelieva0509/planner@dfc69b5598d3a5061d5f26c9258d77a2472c2658 🚀

--- src/components/Layout/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from "react";
-import css from "./Layout.module.scss"
-
-const Layout = ({ children }: { children: ReactNode }) => {
-  return <main className={css.container}>{children}</main>;
-};
-
-export default Layout
--- src/redux/selectors.tsx
+++
@@ -1,7 +0,0 @@
-import { RootState } from '../types';
-
-export const getTasks = (state: RootState) => state.tasks.tasks;
-
-export const getDeletedTasks = (state: RootState) => state.tasks.deletedTasks;
-
-export const getFilter = (state: RootState) => state.filter.status;
